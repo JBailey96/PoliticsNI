a4dca040d2a7ba548f969a8ab5cd397338e7fdf8
App Camp <appcamp@apps-macbook-air-3.local>
2016-07-20 13:35:29 +0100

    Initial Commit

---
+++ Politics NI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Politics NI
+//
+//  Created by App Camp on 20/07/2016.
+//  Copyright © 2016 App Camp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
