db630b284ec67b3391ac3b6aa21f99b9abe260f2
App Camp <appcamp@Apps-MacBook-Air-3.local>
2016-07-27 11:45:56 +0100

    Added sidebar and sorted out login

---
+++ Politics NI/HubViewControlller.swift
@@ -0,0 +1,13 @@
+//
+//  HubViewControlller.swift
+//  Politics NI
+//
+//  Created by App Camp on 27/07/2016.
+//  Copyright © 2016 App Camp. All rights reserved.
+//
+
+import Cocoa
+
+class HubViewControlller: UITableViewController {
+
+}
