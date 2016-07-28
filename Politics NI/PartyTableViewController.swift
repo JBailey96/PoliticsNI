0dc91e1ef231003e346a3eb1712e02e62a1c0664
App Camp <appcamp@Apps-MacBook-Air-3.local>
2016-07-28 14:52:53 +0100

    Added party views

---
+++ Politics NI/PartyTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PartyTableViewController.swift
+//  Politics NI
+//
+//  Created by App Camp on 28/07/2016.
+//  Copyright © 2016 App Camp. All rights reserved.
+//
+
+import Cocoa
+
+class PartyTableViewController: UITableViewController {
+
+}
