c5e774f792b21800dde42537387d7227d0cc901b
App Camp <appcamp@Apps-MacBook-Air-3.local>
2016-08-03 16:27:46 +0100

    Integrate user issues reply selection view

---
+++ Politics NI/IssuesCellViewController.swift
@@ -0,0 +1,13 @@
+//
+//  IssuesCellViewController.swift
+//  Politics NI
+//
+//  Created by App Camp on 03/08/2016.
+//  Copyright © 2016 App Camp. All rights reserved.
+//
+
+import Cocoa
+
+class IssuesCellViewController: UITableViewCell {
+
+}
---
+++ Politics NI/SelectIssuesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SelectIssuesTableViewController.swift
+//  Politics NI
+//
+//  Created by App Camp on 03/08/2016.
+//  Copyright © 2016 App Camp. All rights reserved.
+//
+
+import Cocoa
+
+class SelectIssuesTableViewController: UITableViewController {
+
+}
