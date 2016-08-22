c05a0e5b88020e3b0088c80895fa2cfa2171f88a
App Camp <appcamp@Apps-MacBook-Air-3.local>
2016-08-22 12:00:40 +0100

    User can now remove responded issues + changed what stats are uploaded

---
+++ Politics NI/AboutViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.swift
+//  Politics NI
+//
+//  Created by App Camp on 21/08/2016.
+//  Copyright © 2016 App Camp. All rights reserved.
+//
+
+import Cocoa
+
+class AboutViewController: UITableViewController {
+
+}
